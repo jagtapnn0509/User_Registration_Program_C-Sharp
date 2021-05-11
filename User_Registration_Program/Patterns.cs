d5b72bab50db48d9b816e693ed121fd0006a042e
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-11 21:05:11 +0530

    Add : Function to validate Last name

--- User_Registration_Program/Patterns.cs
+++ User_Registration_Program/Patterns.cs
@@ -9,8 +9,14 @@ namespace User_Registration_Program
     {
         public bool Check_FirstName(String First_Name)
         {
-            String pattern_Name = "^[A-Z]{1}[a-z]{2,}";
-            return Regex.IsMatch(First_Name, pattern_Name);
+            String Pattern_Name = "^[A-Z]{1}[a-z]{2,}";
+            return Regex.IsMatch(First_Name, Pattern_Name);
+        }
+
+        public bool Check_LastName(String Last_Name)
+        {
+            String Pattern_Name = "^[A-Z]{1}[a-z]{2,}";
+            return Regex.IsMatch(Last_Name, Pattern_Name);
         }
     }
 }
--- User_Registration_Program/Program.cs
+++ User_Registration_Program/Program.cs
@@ -20,16 +20,33 @@ namespace User_Registration_Program
             {
                 Console.WriteLine("First Name is Invalid");
             }
+            Console.WriteLine("----------------------------------------------------");
+        }
+        public void valid_LastName()
+        {
+            Patterns pattern = new Patterns();
+            Console.Write("Enter The Last Name : ");
+            String Last_Name = (Console.ReadLine());
+            Boolean Regex_Name = pattern.Check_LastName(Last_Name);
+
+            if (Regex_Name)
+            {
+                Console.WriteLine("Last Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("Last Name is Invalid");
+            }
+            Console.WriteLine("----------------------------------------------------");
         }
        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
+            Console.WriteLine("----------------------------------------------------");
             Program program = new Program();
             program.Valid_FirstName();
-           
-
-           
+            program.valid_LastName();
         }
     }
 }
