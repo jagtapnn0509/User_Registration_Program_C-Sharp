250cb34a686b618becf3ce2a8ce33686517660d8
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-11 18:14:56 +0530

    Add : Welcome Message

---
+++ User_Registration_Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User_Registration_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+        }
+    }
+}
