3beb279ae0262578d734d72149a87111e03ca6a1
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-11 20:52:34 +0530

    Add : function to validate First name

---
+++ User_Registration_Program/Patterns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace User_Registration_Program
+{
+    class Patterns
+    {
+        public bool Check_FirstName(String First_Name)
+        {
+            String pattern_Name = "^[A-Z]{1}[a-z]{2,}";
+            return Regex.IsMatch(First_Name, pattern_Name);
+        }
+    }
+}
--- User_Registration_Program/Program.cs
+++ User_Registration_Program/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace User_Registration_Program
 {
     class Program
     {
+        public void Valid_FirstName()
+        {
+            Patterns pattern = new Patterns();
+            Console.Write("Enter The First Name : ");
+            String First_Name = (Console.ReadLine());
+            Boolean Regex_Name = pattern.Check_FirstName(First_Name);
+           
+            if(Regex_Name)
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is Invalid");
+            }
+        }
+       
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
+            Program program = new Program();
+            program.Valid_FirstName();
+           
+
+           
         }
     }
 }
